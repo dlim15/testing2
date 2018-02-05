be99498b40e25c7a5e888f45a07840cdf1386d0c
dlim15 <d_lim10@u.pacific.edu>
2018-02-05 10:08:56 -0800

    Create GeneralFuncs.groovy

---
+++ GeneralFuncs.groovy
@@ -0,0 +1,18 @@
+#!groovy
+
+def database_command_create( pass, host, port, user ){
+  return pass + "|psql --host=" + host + " --port=" + port + " --username=" + user + " --password --dbname onostest -c "
+}
+def basicGraphPart( rFileName, host, port, user, pass, subject, branchName ){
+  return " Rscript " + rFileName + " " + host + " " + port + " " + user + " " + pass + " " + subject + " " + branchName
+}
+def initBasicVars(){
+  rScriptLocation = "~/OnosSystemTest/TestON/JenkinsFile/scripts/"
+}
+def generateTestList( tests ){
+    testList = ""
+    for( String test : tests.keySet() )
+        testList += test + ","
+    testList = testList[ 0..-2 ]
+}
+return this;
