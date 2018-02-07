2b0c7e90786cb2ba272d1e9ac43b221bc19f0c50
dlim15 <d_lim10@u.pacific.edu>
2018-02-07 12:32:44 -0800

    Update JenkinsCommonFuncs.groovy

--- JenkinsCommonFuncs.groovy
+++ JenkinsCommonFuncs.groovy
@@ -305,6 +305,7 @@ def generateOverallGraph( prop, testCategory, graph_saved_directory ){
           string( credentialsId: 'db_host', variable: 'host' ),
           string( credentialsId: 'db_port', variable: 'port' ) ] ) {
               print generalFuncs.basicGraphPart( trend_generator_file, host, port, user, pass, testType, prop[ "ONOSBranch" ] ) + " " + generalFuncs.testList + " 20 " + graph_saved_directory
+              generalFuncs.generateTestList( testCategory )
               sh '''#!/bin/bash
               ''' + generalFuncs.basicGraphPart( trend_generator_file, host, port, user, pass, testType, prop[ "ONOSBranch" ] ) + " " + generalFuncs.testList + " 20 " + graph_saved_directory + '''
               ''' + getOverallTrendLine( host, port, user, pass, prop, "pass" ) + '''
